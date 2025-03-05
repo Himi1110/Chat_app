745c1c4a106386eb753eb2a3c3f25c6e14028d92
minh <orderhimi@gmail.com>
2025-03-05 09:45:00 +0700

    minh

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-app.use(express.json())
-app.use("api/auth", authRoutes)
+app.use(express.json());
+app.use("api/auth", authRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
-  connectDB()
+  connectDB();
 });
